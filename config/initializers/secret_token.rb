cec15b33bcb71e5f0f6e7d63074e67ffeb4f37d1
toshipon <toshipon123@gmail.com>
2013-11-13 22:39:40 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FindingStarbucks::Application.config.secret_key_base = 'bd737610d627068ea6c230af987a31dd4faf4957034d48395a7b675519c4c5545a4d6c02ace559dd908ac2b069feaeff996e56f8938c022355eacc7d0ab7b0d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FindingStarbucks::Application.config.session_store :cookie_store, key: '_finding-starbucks_session'

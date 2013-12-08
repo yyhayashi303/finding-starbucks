603fb77119f9310bd47d2fac64457d5f44eb1e6a
yyhayashi303 <yy.hayashi303@gmail.com>
2013-12-08 16:07:22 +0900

    カラム名をstore_idに変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203124147) do
+ActiveRecord::Schema.define(version: 20131208070120) do
 
   create_table "stores", force: true do |t|
     t.string   "name"
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 20131203124147) do
     t.decimal  "lat"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "sotreId"
+    t.integer  "store_id"
   end
 
-  add_index "stores", ["sotreId"], name: "index_stores_on_sotreId"
+  add_index "stores", ["store_id"], name: "index_stores_on_store_id"
 
 end
